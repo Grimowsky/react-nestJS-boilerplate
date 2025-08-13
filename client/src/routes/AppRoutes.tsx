172f0fdac3ed97414371bfc9834ef5059b224cad
Rafal Mirkowski <mirkowsr@air.local>
2025-08-13 12:48:38 +0200

    setup prettier in client

--- client/src/routes/AppRoutes.tsx
+++ client/src/routes/AppRoutes.tsx
@@ -5,12 +5,7 @@ import { Route, Routes } from 'react-router-dom';
 export const AppRoutes = (): React.ReactElement => {
   return (
     <Routes>
-      <Route
-        path={routes.BASE_ROUTE}
-        element={
-          <div className={'h-screen text-black-500 text-2xl'}>hello there</div>
-        }
-      />
+      <Route path={routes.BASE_ROUTE} element={<div>hello</div>} />
     </Routes>
   );
 };
