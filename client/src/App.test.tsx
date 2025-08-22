e17bf749a2af9206cb5bf1c918646d86c235efa4
Rafal Mirkowski <mirkowsr@air.local>
2025-08-22 10:27:30 +0200

    add working  vitest

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,11 +1,11 @@
 import { App } from './App';
 import { render, screen } from '@testing-library/react';
-import { describe, it } from 'vitest';
+import { describe, it, expect } from 'vitest';
 
 //TODO: remove this test  after forking repo
 describe('App basic test', () => {
   it('Should render greetings', () => {
     render(<App />);
-    expect(screen.getByText('hello there')).toBeDefined();
+    expect(screen.getByText('hello')).toBeDefined();
   });
 });
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,29 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-    plugins: [react(), svgr()],
-    server: {
-        host: true,
-        port: 3000,
-        watch: {
-            usePolling: true,
-            interval: 500
-        }
+  plugins: [react(), svgr()],
+  server: {
+    host: true,
+    port: 3000,
+    watch: {
+      usePolling: true,
+      interval: 500,
     },
-    preview: {
-        host: true,
-        port: 3000
+  },
+  preview: {
+    host: true,
+    port: 3000,
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './testSetup.js',
+    coverage: {
+      provider: 'istanbul',
+      reporter: ['text', 'json', 'html', 'lcov'],
     },
-    test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './testSetup.js',
-        coverage: {
-            provider:'istanbul',
-            reporter: ['text', 'json', 'html', 'lcov'],
-        }
-    },
-})
+  },
+});
