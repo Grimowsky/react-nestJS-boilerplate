32235dfe33867e8c4e5dd9e608ddea929a46607c
Rafal Mirkowski <rafalmirkowski@gmail.com>
2025-09-01 17:37:41 +0200

    change client code

--- client/src/routes/routes.schema.ts
+++
@@ -1,3 +0,0 @@
-export const routes = {
-  BASE_ROUTE: '/',
-};
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,29 +1,7 @@
-/// <reference types="vitest" />
-import { defineConfig } from 'vite';
-import svgr from 'vite-plugin-svgr';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  server: {
-    host: true,
-    port: 3000,
-    watch: {
-      usePolling: true,
-      interval: 500,
-    },
-  },
-  preview: {
-    host: true,
-    port: 3000,
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './testSetup.js',
-    coverage: {
-      provider: 'istanbul',
-      reporter: ['text', 'json', 'html', 'lcov'],
-    },
-  },
-});
+  plugins: [react()],
+})
