9bf65010cf1b0841665621abf1ce15a47c6472a5
Rafal Mirkowski <rafalmirkowski@gmail.com>
2025-09-02 07:12:44 +0200

    fix server nest js config

--- server-nestjs/src/common/zod.validation.pipe.ts
+++ server-nestjs/src/common/zod.validation.pipe.ts
@@ -1,19 +1,15 @@
-import {
-  ArgumentMetadata,
-  BadRequestException,
-  PipeTransform,
-} from '@nestjs/common';
+import { BadRequestException, PipeTransform } from '@nestjs/common';
 import { ZodSchema } from 'zod';
 
 export class ZodValidationPipe implements PipeTransform {
   constructor(private schema: ZodSchema) {}
 
-  transform(value: any, metadata: ArgumentMetadata): any {
+  transform(value: any): any {
     try {
-      console.log('i wass here validation');
+      console.log('i was here validation');
       return this.schema.parse(value);
     } catch (e) {
-      console.log('i was here - catch');
+      console.log('i was here - catch', e);
       throw new BadRequestException('Request validation failed');
     }
   }
